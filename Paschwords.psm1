ff7713909554c9d47f299a2ec5ecfdf6c47fc030
Schvenn <42421399+Schvenn@users.noreply.github.com>
2025-07-06 22:41:15 -0400

    v4.62

--- Paschwords.psm1
+++ Paschwords.psm1
@@ -2765,7 +2765,7 @@ Privileged Users:
 ## License
 MIT License
 
-Copyright (c) 2025 Schvenn
+Copyright (c) 2025 Craig Plath
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
